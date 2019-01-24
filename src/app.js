5ed59e9817efc2710a86ef2c3f42423bc8d3cb94
Rory Dungan <rorydungan@gmail.com>
2019-01-24 23:44:45 +1000

    Start work on metro line calculation

--- src/app.js
+++ src/app.js
@@ -2,51 +2,36 @@
 // Source code licensed under Apache License 2.0.
 // Copyright © 2017 William Ngan. (https://github.com/williamngan/pts)
 
-import {CanvasSpace, Create, Polygon} from 'pts'
+import {CanvasSpace, Create, Pt, Polygon} from 'pts'
 
 
 // Initiate Space and Form
 const space = new CanvasSpace('#pts')
-  .setup({ bgcolor: '#09F', resize: true, retina: true })
+  .setup({ bgcolor: '#fff', resize: true, retina: true })
 const form = space.getForm()
 
-let landmarks
+console.log('test')
+
+const colour = '#42e'
 
 space.add({
 
   start: (bound) => {
-    // Make a face with 30 radial points with slight randomness
-    let radius = space.size.minValue().value/3
-    landmarks = Create.radialPts(space.center, radius, 30)
-    landmarks.map(p => p.add(50*(Math.random() - Math.random())))
-
   },
 
   animate: (time, ftime) => {
+    const startPoint = space.center
+    const endPoint = space.pointer
 
-    landmarks[landmarks.length-1] = space.pointer
+    const sign = Math.sign(endPoint.y - startPoint.y)
 
-    // convex hull the points
-    let hull = Polygon.convexHull(landmarks)
+    const curveStart = Math.tan(sign * Math.PI / 4) * Math.abs(endPoint.x - startPoint.x)
 
-    // eyes' positions
-    let left = space.center.$subtract(50)
-    let right = space.center.$add(50, -50)
-    let leftB = left.clone().toAngle(space.pointer.$subtract(left).angle(), 10, left)
-    let rightB = right.clone().toAngle(space.pointer.$subtract(right).angle(), 10, right)
+    // console.log(curveStart)
 
-    // draw face and eyes
-    form.fillOnly('rgba(255, 255, 255, 0.5)').polygon(hull)
-    form.fill('#fff').points([left, right], 20, 'circle')
-    form.fill('#123').points([leftB, rightB], 5, 'circle')
-
-    // draw the hull and pts
-    form.fill('#fff').points(hull, 5, 'circle')
-    form.fill('rgba(0,0,0,.5)').points(landmarks, 2, 'circle')
-    form.fill('#f03').point(space.pointer, 10, 'circle')
-
-    // draw mouth
-    form.strokeOnly('#123', 5).line([left.$add(0,80), right.$add(0, 80)])
+    const midpoint = new Pt(startPoint.x, endPoint.y - curveStart)
+    form.stroke(colour, 5).line([startPoint, midpoint])
+    form.stroke(colour, 5).line([midpoint, endPoint])
   },
 })
 
