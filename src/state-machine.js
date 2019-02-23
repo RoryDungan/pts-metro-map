70eb57b56b952b4ebb10073571ced122e937dd66
Rory Dungan <rorydungan@gmail.com>
2019-02-23 18:58:57 +1000

    Add simple state machine

---
+++ src/state-machine.js
@@ -0,0 +1,29 @@
+'use strict'
+
+export const createStateMachine = rootState => {
+  const stateStack = [rootState]
+
+  const stateMachine = {
+    push: state => {
+      stateStack.push(state)
+      if (typeof state.enter === 'function') {
+        state.enter(stateMachine)
+      }
+    },
+    pop: () => {
+      const oldState = stateStack.pop()
+      if (typeof oldState.exit === 'function') {
+        oldState.exit(stateMachine)
+      }
+    },
+    update: () => {
+      for (let i = stateStack.length - 1; i >= 0; i--) {
+        const currentState = stateStack[i]
+        if (typeof currentState.update === 'function') {
+          currentState.update(stateMachine)
+        }
+      }
+    }
+  }
+  return stateMachine
+}
